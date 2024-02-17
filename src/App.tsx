156a1c86e40f8dfe42c431c5370ac64b89af8c7d
Annatar <itsAnnatar@gmail.com>
2024-02-17 19:47:34 +0330

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <h1>hello world</h1>;
+  const count = (a: number, b: number) => {
+    return a + b;
+  };
+  return (
+    <>
+      <h1>hello world</h1>;<div>{count(4, 6)}</div>
+    </>
+  );
 };
 
 export default App;
